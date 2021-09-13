8ddab9895cd0120a4fb73343a9115f1ecd6d977d
Keidusii <mhallii@umich.edu>
2021-09-13 16:19:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Main from './components/MainComponent'
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+class App extends Component {
+    render() {
+        const store = ConfigureStore();
+        
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div className="App">
+                        <Main />
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        );
+    };
+}
+
+export default App;
